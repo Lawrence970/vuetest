d2b354c61f4d9140b9aa41fea884883b688abc7e
Raider144 <lawrence.isaac970@gmail.com>
2021-07-26 14:55:37 -0600

    added vue-router, componentizing .html from trucker_mile_tracker dir

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
-<!-- ??? This file is where everything gets compiled, it then goes to index.html to be injected -->
 <template>
-  <loginForm/>
- 
+  
 </template>
 
-<script>
-
-// * this is where all of the components you want are IMPORTED
-import loginForm from './components/loginForm.vue'
-
-
-// * everything in the application is 'stored' here in the export default option/object
-export default {
-  name: 'App',
-  // * this is where all of the components are EXPORTED; injected into index.html
-  components: {
-    loginForm,
-    
-  }
-}
-</script>
-
 <style>
-/* ? this styling for the div container with id=app */
-
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  margin:0 auto;
-  padding:0 auto;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
--- src/components/loginForm.vue
+++ src/components/loginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>hellow</p>
+    
   </div>
 
 </template>
